48a119cff18e73c33de7a81444c15470df089a52
Rafael Barbosa <rafael_barbosa2@hotmail.com>
2023-08-07 17:38:03 -0300

    main

--- fii.py
+++ fii.py
@@ -3,7 +3,7 @@ import dash
 from dash import dcc, html
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
-import pandas as pd  # Import the pandas library to read CSV files
+import pandas as pd
 
 def obter_valor_atual(fundo):
     ticker = f"{fundo}.SA"
@@ -15,15 +15,16 @@ def obter_valor_atual(fundo):
     except:
         return None
 
-def calcular_ponto_medio(valor_compra, valor_venda):
-    return (valor_compra + valor_venda) / 2
+def calcular_ponto_medio(compras):
+    total_valor = sum(compra['Total'] for compra in compras)
+    total_cotas = sum(compra['Quantidade'] for compra in compras)
+    return total_valor / total_cotas
 
 def calcular_lucro_prejuizo(valor_compra, valor_venda, quantidade_cotas):
     return (valor_venda - valor_compra) * quantidade_cotas
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.CYBORG, "style.css"])
 
-# Read the CSV file and populate the fund options dynamically
 csv_file_path = "fundosListados.csv"
 fund_options_df = pd.read_csv(csv_file_path, delimiter=";", usecols=["Código"], encoding="ISO-8859-1")
 fund_options_df.rename(columns={"Código": "value"}, inplace=True)
@@ -36,29 +37,73 @@ app.layout = dbc.Container([
             html.Label("Selecione o Fundo:", className="label"),
             dcc.Dropdown(id="input-fundo", options=[{"label": fundo, "value": fundo} for fundo in fund_options], value="", className="input-field")
         ]),
-        dbc.Col([
-            html.Label("Valor de Compra:", className="label_compra"),
-            dcc.Input(id="input-valor-compra", type="number", value="", className="input-field")
+    ]),
+    dbc.Card(
+        dbc.CardBody([
+            dbc.Row([
+                dbc.Col([
+                    html.Label("Compra 1 - Valor:", className="label_compra"),
+                    dcc.Input(id="input-valor-compra", type="number", value="", className="input-field")
+                ]),
+                dbc.Col([
+                    html.Label("Compra 1 - Cotas:", className="label_compra"),
+                    dcc.Input(id="input-cotas-compradas", type="number", value="", className="input-field")
+                ]),        
+            ]),
+            dbc.Row([
+                dbc.Col([
+                    html.Label("Compra 2 - Valor:", className="label_compra"),
+                    dcc.Input(id="input-valor-compra-2", type="number", value="", className="input-field")
+                ]),
+                dbc.Col([
+                    html.Label("Compra 2 - Cotas:", className="label_compra"),
+                    dcc.Input(id="input-cotas-compradas-2", type="number", value="", className="input-field")
+                ]),        
+            ]),
+            dbc.Row([
+                dbc.Col([
+                    html.Label("Compra 3 - Valor:", className="label_compra"),
+                    dcc.Input(id="input-valor-compra-3", type="number", value="", className="input-field")
+                ]),
+                dbc.Col([
+                    html.Label("Compra 3 - Cotas:", className="label_compra"),
+                    dcc.Input(id="input-cotas-compradas-3", type="number", value="", className="input-field")
+                ]),        
+            ]),
+            dbc.Row([
+                dbc.Col([
+                    html.Label("Compra 4 - Valor:", className="label_compra"),
+                    dcc.Input(id="input-valor-compra-4", type="number", value="", className="input-field")
+                ]),
+                dbc.Col([
+                    html.Label("Compra 4 - Cotas:", className="label_compra"),
+                    dcc.Input(id="input-cotas-compradas-4", type="number", value="", className="input-field")
+                ]),        
+            ]),
         ]),
-        dbc.Col([
-            html.Label("Cotas Compra:", className="label_compra"),
-            dcc.Input(id="input-cotas-compradas", type="number", value="", className="input-field")
-        ]),        
-        dbc.Col([
-            html.Label("Valor de Venda:", className="label_venda"),
-            dcc.Input(id="input-valor-venda", type="number", value="", className="input-field")
+        className="mt-3"
+    ),
+    dbc.Card(
+        dbc.CardBody([
+            dbc.Row([
+                dbc.Col([
+                    html.Label("Valor de Venda:", className="label_venda"),
+                    dcc.Input(id="input-valor-venda", type="number", value="", className="input-field")
+                ]),
+                dbc.Col([
+                    html.Label("Cotas Venda:", className="label_venda"),
+                    dcc.Input(id="input-cotas-venda", type="number", value="", className="input-field")
+                ]),
+            ]),
+            dbc.Row([
+                dbc.Col([
+                    dbc.Button("Calcular", id="calcular-button", n_clicks=0, color="primary", className="mt-4")
+                ])
+            ], className="col"),
         ]),
-        dbc.Col([
-            html.Label("Cotas Venda:", className="label_venda"),
-            dcc.Input(id="input-cotas-venda", type="number", value="", className="input-field")
-        ]),
-        dbc.Col([
-            dbc.Button("Calcular", id="calcular-button", n_clicks=0, color="primary", className="mt-4")
-        ])
-    ], className="col"),
+        className="mt-3"
+    ),
     html.Div(id="resultado-container", className="resultado-container"),
-
-    # Footer
     dbc.NavbarSimple(
         children=[
             dbc.NavItem("Desenvolvido por Rafael Barbosa | 2023", className="footer")
@@ -76,15 +121,27 @@ app.layout = dbc.Container([
         dash.dependencies.State("input-fundo", "value"),
         dash.dependencies.State("input-valor-compra", "value"),
         dash.dependencies.State("input-cotas-compradas", "value"),        
+        dash.dependencies.State("input-valor-compra-2", "value"),
+        dash.dependencies.State("input-cotas-compradas-2", "value"),
+        dash.dependencies.State("input-valor-compra-3", "value"),
+        dash.dependencies.State("input-cotas-compradas-3", "value"),
+        dash.dependencies.State("input-valor-compra-4", "value"),
+        dash.dependencies.State("input-cotas-compradas-4", "value"),
         dash.dependencies.State("input-valor-venda", "value"),
         dash.dependencies.State("input-cotas-venda", "value"),
     ],
 )
-def calcular_e_mostrar_resultado(n_clicks, fundo, valor_compra, valor_venda, cotas_compradas, cotas_venda):
+def calcular_e_mostrar_resultado(n_clicks, fundo, valor_compra, cotas_compradas, valor_compra_2, cotas_compradas_2, valor_compra_3, cotas_compradas_3, valor_compra_4, cotas_compradas_4, valor_venda, cotas_venda):
     if n_clicks > 0:
         valor_atual = obter_valor_atual(fundo)
         if valor_atual is not None:
-            ponto_medio = calcular_ponto_medio(valor_compra, valor_venda)
+            compras = [
+                {'Quantidade': cotas_compradas, 'Total': valor_compra},
+                {'Quantidade': cotas_compradas_2, 'Total': valor_compra_2},
+                {'Quantidade': cotas_compradas_3, 'Total': valor_compra_3},
+                {'Quantidade': cotas_compradas_4, 'Total': valor_compra_4},
+            ]
+            ponto_medio = calcular_ponto_medio(compras)
             lucro_prejuizo_compradas = calcular_lucro_prejuizo(valor_compra, valor_atual, cotas_compradas)
             lucro_prejuizo_venda = calcular_lucro_prejuizo(valor_venda, valor_atual, cotas_venda)
 
