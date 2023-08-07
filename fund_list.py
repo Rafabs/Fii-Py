cc906ee5d99a6281501afb6745b7e1e4f7cb96e9
Rafael Barbosa <rafael_barbosa2@hotmail.com>
2023-08-07 15:35:28 -0300

    main

---
+++ fund_list.py
@@ -0,0 +1,6 @@
+fund_options = [
+    {'label': 'Fund A', 'value': 'FUND_A'},
+    {'label': 'Fund B', 'value': 'FUND_B'},
+    {'label': 'Fund C', 'value': 'FUND_C'},
+    # Add more funds as needed
+]
